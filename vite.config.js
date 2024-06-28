4b7ec87c28e3c14532b082fab957cb1509b7337f
thanigaivel14 <thanigai905@gmail.com>
2024-06-28 22:06:03 +0530

    404 error

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'thaniagi14/React-todo-app',
+  base:'/thaniagi14/React-todo-app/',
 })
